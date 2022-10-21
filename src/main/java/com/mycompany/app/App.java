21dbcadbb99e4d2ae4441b2b0835e47b00f08622
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2022-10-21 15:19:55 +0200

    Fix code smell

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-     System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
